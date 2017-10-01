e9ff5ebcdf47f329b635a4c466cb7c7e647b04b1
biscu <biscu@live.it>
2017-10-01 21:04:56 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+function setup() {
+  createCanvas(800, 600);
+  background(204,204,204);
+  
+
+}
+
+function draw() {
+  //Bottom Semi Circle
+  fill(102,102,102);
+  stroke(102,102,102);
+  ellipse(200,300,65,65);
+  
+   //Line D
+ 
+  fill(102,102,102);
+  line(180,40,215,80);
+  
+  //Line D
+ 
+  fill(102,102,102);
+  line(280,80,245,75);
+  
+   //Line D
+ 
+  fill(102,102,102);
+  line(230,0,205,100);
+  
+  //Line2
+ 
+  fill(102,102,102);
+  rect(216,70,1,200);
+  
+  //Line3
+ 
+  fill(102,102,102);
+  rect(227,70,1,200);
+  
+  //Line4
+ 
+  fill(102,102,102);
+  rect(205,70,1,200);
+  
+  //Black Body
+  fill(0,0,0);
+  noStroke();
+  rect(155,170,90,130);
+  
+  //Grey Line
+  fill(102,102,102);
+  rect(155,190,90,6);
+  
+  //LIne 1
+  
+  //Line 2
+  
+  //Line 3
+  
+  //Cricle Black Head
+  fill(0,0,0);
+  ellipse(220,75,85,85);
+  
+  //EYE
+  fill(255,255,255);
+  ellipse(230,70,30,30);
+  
+  //Pupilla
+  fill(0,0,0);
+  ellipse(230,70,7,7);
+  
+   //Neo1
+  fill(152,152,152);
+  ellipse(205,68,10,10);
+  
+  //Neo2
+  fill(152,152,152);
+  ellipse(245,85,6,6);
+  
+   //Neo3
+  fill(152,152,152);
+  ellipse(235,48,8,8);
+  
+ 
+  
+  
+  
+}
